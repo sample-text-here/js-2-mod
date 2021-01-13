3ccffd045639338196460e64d3a659dec8224ac6
Glitch (js-2-mod) <none>
2021-01-13 23:04:29 +0000

    📈🎮 Updated with Glitch

--- server.js
+++ server.js
@@ -138,6 +138,6 @@ function format(obj) {
 }
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, () => {
+var listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
 });
